d3a0da74493ecf9333a7dad79b545f01f763ef28
Memoristor <1183827192@qq.com>
2022-12-05 12:04:03 +0800

    add cmake support for CUDA

--- include/utils.h
+++ include/utils.h
@@ -1,25 +1,16 @@
 #ifndef __CUDA_UTILS_H
 #define __CUDA_UTILS_H
 
+#include <torch/torch.h>
 #include <torch/extension.h>
+#include <cuda.h>
 #include <cuda_runtime.h>
 
 #if defined(__cplusplus)
 extern "C" {
 #endif
 
-#define CHECK(call)                                                         \
-{                                                                           \
-    const cudaError_t error = call;                                         \  
-    if (error != cudaSuccess)                                               \
-    {                                                                       \
-        printf("Error: %s, %d, ", __FILE__, __LINE__);                      \
-        printf("cuda: %d, reason: %s\n", error, cudaGetErrorString(error)); \
-        exit(1);                                                            \
-    }                                                                       \
-} 
-
-#define CHECK_CUDA(x) TORCH_CHECK(x.type().is_cuda(), #x " must be a CUDA tensor")
+#define CHECK_CUDA(x) TORCH_CHECK(x.is_cuda(), #x " must be a CUDA tensor")
 #define CHECK_CONTIGUOUS(x) TORCH_CHECK(x.is_contiguous(), #x " must be contiguous")
 #define CHECK_INPUT(x) CHECK_CUDA(x); CHECK_CONTIGUOUS(x)
 
