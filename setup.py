9801a6af7f75acbc49b825f4a90956f46932a22d
Memoristor <1183827192@qq.com>
2022-12-07 15:21:39 +0800

    reconfig setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
 from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+from glob import glob
+import os
 
 setup(
     name='fdstlib',
     version="0.1.0",
     author="cjdeng",
     ext_modules=[
-        CUDAExtension('fdstlib', [
-            'torch_dct.cpp',
-            'torch_dct_kernel.cu',
-        ])
+        CUDAExtension('fdstlib', glob(os.path.join('source', '*.c*')))
+    ],
+    include_dirs=[
+        'include',
     ],
     cmdclass={
         'build_ext': BuildExtension
