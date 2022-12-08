06eae5aab938b694fe64e562a888f8302db1261a
Memoristor <1183827192@qq.com>
2022-12-08 19:01:53 +0800

    modify torch_dct.cpp

--- source/torch_dct.cpp
+++ source/torch_dct.cpp
@@ -3,22 +3,17 @@
 
 #include "../include/utils.h"
 
-at::Tensor cudaNativeDCTII2D(const at::Tensor input, const uint numPoints);
-at::Tensor cudaNativeIDCTII2D(const at::Tensor input, const uint numPoints);
+at::Tensor cudaNativeDCT2D(const at::Tensor input, const uint numPoints);
+at::Tensor cudaNativeIDCT2D(const at::Tensor input, const uint numPoints);
 
-at::Tensor nativeDCTII2D(const at::Tensor input, const uint numPoints, const bool sortCoff) {
+at::Tensor nativeDCT2D(const at::Tensor input, const uint numPoints, const bool sortCoff) {
     CHECK_4DTENSOR(input);
     at::Tensor padInput = zeroPadInputTensorToFitPointSize(input, numPoints);
-    return cudaNativeDCTII2D(padInput, numPoints);
+    return cudaNativeDCT2D(padInput, numPoints);
 }
 
-at::Tensor nativeIDCTII2D(const at::Tensor input, const uint numPoints, const bool sortCoff) {
+at::Tensor nativeIDCT2D(const at::Tensor input, const uint numPoints, const bool sortCoff) {
     CHECK_4DTENSOR(input);
     at::Tensor padInput = zeroPadInputTensorToFitPointSize(input, numPoints);
-    return cudaNativeIDCTII2D(padInput, numPoints);
-}
-
-PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-  m.def("nativeDCTII2D", &nativeDCTII2D, "Native 2D DCT-II (CUDA)");
-  m.def("nativeIDCTII2D", &nativeIDCTII2D, "Native 2D IDCT-II (CUDA)");
+    return cudaNativeIDCT2D(padInput, numPoints);
 }
