c3bff9f58abb464bf168b9b0a2d65b75ac00c906
Memoristor <1183827192@qq.com>
2022-12-08 19:03:02 +0800

    add pybind.cpp

---
+++ source/pybind.cpp
@@ -0,0 +1,16 @@
+
+#include "../include/utils.h"
+
+at::Tensor nativeDCT2D(const at::Tensor input, const uint numPoints, const bool sortCoff);
+at::Tensor nativeIDCT2D(const at::Tensor input, const uint numPoints, const bool sortCoff);
+
+at::Tensor nativeDST2D(const at::Tensor input, const uint numPoints, const bool sortCoff);
+at::Tensor nativeIDST2D(const at::Tensor input, const uint numPoints, const bool sortCoff);
+
+PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
+  m.def("nativeDCT2D", &nativeDCT2D, "Native 2D-DCT (CUDA)");
+  m.def("nativeIDCT2D", &nativeIDCT2D, "Native 2D-IDCT (CUDA)");
+
+  m.def("nativeDST2D", &nativeDST2D, "Native 2D-DST (CUDA)");
+  m.def("nativeIDST2D", &nativeIDST2D, "Native 2D-IDST (CUDA)");
+}
